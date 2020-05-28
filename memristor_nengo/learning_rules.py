7bca228f5284638c820ab4e70df342f41ba6d56a
Thomas <thomas.tiotto@outlook.com>
2020-05-28 11:13:46 +0200

    - vectorized update rule

--- memristor_nengo/learning_rules.py
+++ memristor_nengo/learning_rules.py
@@ -20,8 +20,10 @@ class mPES( LearningRuleType ):
     
     learning_rate = NumberParam( "learning_rate", low=0, readonly=True, default=1e-4 )
     pre_synapse = SynapseParam( "pre_synapse", default=Lowpass( tau=0.005 ), readonly=True )
+    r_max = NumberParam( "r_max", readonly=True, default=2.5e8 )
+    r_min = NumberParam( "r_min", readonly=True, default=1e2 )
     
-    def __init__( self, memristor_model, learning_rate=Default, pre_synapse=Default ):
+    def __init__( self, learning_rate=Default, pre_synapse=Default, r_max=Default, r_min=Default ):
         super().__init__( learning_rate, size_in="post_state" )
         if learning_rate is not Default and learning_rate >= 1.0:
             warnings.warn(
@@ -30,7 +32,12 @@ class mPES( LearningRuleType ):
                     )
         
         self.pre_synapse = pre_synapse
-        self.memristor_model = memristor_model
+        self.r_max = r_max
+        self.r_min = r_min
+    
+    @staticmethod
+    def initial_normalized_conductances( low, high, shape ):
+        return np.random.randint( low, high, shape )
     
     @property
     def _argdefaults( self ):
@@ -42,43 +49,46 @@ class mPES( LearningRuleType ):
 
 class SimmPES( Operator ):
     def __init__(
-            self, pre_filtered, error, delta, learning_rate, encoders, memristor_model, memristors, tag=None
+            self,
+            pre_filtered,
+            error,
+            delta,
+            learning_rate,
+            encoders,
+            weights,
+            tag=None
             ):
         super( SimmPES, self ).__init__( tag=tag )
         
         self.learning_rate = learning_rate
-        self.memristor_model = memristor_model
-        self.memristors = memristors
-        
-        # create memristor array that implement the weights
-        self.memristors = np.empty_like( delta.initial_value, dtype=object )
-        for i in range( self.memristors.shape[ 0 ] ):
-            for j in range( self.memristors.shape[ 1 ] ):
-                self.memristors[ i, j ] = self.memristor_model()
         
         self.sets = [ ]
         self.incs = [ ]
-        self.reads = [ pre_filtered, error, encoders ]
+        self.reads = [ pre_filtered, error, encoders, weights ]
         self.updates = [ delta ]
     
     @property
-    def delta( self ):
-        return self.updates[ 0 ]
-    
-    @property
-    def encoders( self ):
-        return self.reads[ 2 ]
+    def pre_filtered( self ):
+        return self.reads[ 0 ]
     
     @property
     def error( self ):
         return self.reads[ 1 ]
     
     @property
-    def pre_filtered( self ):
-        return self.reads[ 0 ]
+    def encoders( self ):
+        return self.reads[ 2 ]
+    
+    @property
+    def weights( self ):
+        return self.reads[ 3 ]
+    
+    @property
+    def delta( self ):
+        return self.updates[ 0 ]
     
     def _descstr( self ):
-        return "pre=%s, error=%s -> %s" % (self.pre_filtered, self.error, self.delta)
+        return "pre=%s, error=%s, weights=%s -> %s" % (self.pre_filtered, self.error, self.weights, self.delta)
     
     def make_step( self, signals, dt, rng ):
         pre_filtered = signals[ self.pre_filtered ]
@@ -87,35 +97,47 @@ class SimmPES( Operator ):
         n_neurons = pre_filtered.shape[ 0 ]
         alpha = -self.learning_rate * dt / n_neurons
         encoders = signals[ self.encoders ]
-        memristors = self.memristors
+        weights = signals[ self.weights ]
         
-        def find_spikes( input_activities, output_activities=None ):
+        def find_spikes( input_activities, shape, output_activities=None ):
+            input_size = shape[ 1 ]
+            output_size = shape[ 0 ]
+            
             spiked_pre = np.tile(
-                    np.array( np.rint( input_activities ), dtype=bool ), (input_activities.shape[ 0 ], 1)
+                    np.array( np.rint( input_activities ), dtype=bool ), (output_size, 1)
                     )
             spiked_post = np.tile(
                     np.expand_dims(
-                            np.array( np.rint( output_activities ), dtype=bool ), axis=1 ),
-                    (1, input_activities.shape[ 0 ])
-                    ) if output_activities is not None else np.ones( (1, input_activities.shape[ 0 ]) )
+                            np.array( np.rint( output_activities ), dtype=bool ), axis=1 ), (1, input_size)
+                    ) if output_activities is not None else np.ones( (1, input_size) )
             
             return np.logical_and( spiked_pre, spiked_post )
         
-        # def memristor_sim( learning_signal ):
-        
         def step_simmpes():
+            # TODO pass parameters or equations/functions directly
+            # TODO keep track of pulse numbers in a Signal
+            a = 1e-3
+            c = 1e-3
+            r_min = 1e2
+            r_max = 2.5e8
+            r_3 = 1e9
+            
+            # calculate the magnitude of the update based on PES learning rule
             local_error = alpha * np.dot( encoders, error )
-            learning_signal = np.outer( local_error, pre_filtered )
+            pes_delta = np.outer( local_error, pre_filtered )
             
-            spiked_map = find_spikes( pre_filtered )
+            # find neurons that spiked and set the other updates to zero
+            # spiked_map = find_spikes( pre_filtered, pes_delta.shape )
+            # np.logical_and( pes_delta, spiked_map, out=pes_delta )
             
-            if spiked_map.any():
-                for j, i in np.transpose( np.where( spiked_map ) ):
-                    update = self.memristors[ j, i ].pulse( learning_signal[ j, i ] )
-                    # update = update if update >=
-                    delta[ j, i ] = update
-            
-            # np.outer( alpha * np.dot( encoders, error ), pre_filtered, out=delta )
+            # set update direction and magnitude (unused with powerlaw memristor equations)
+            V = np.sign( pes_delta ) * 1e-1
+            # use the correct equation for the bidirectional powerlaw memristor update
+            try:
+                delta[ V > 0 ] = r_min + r_max * ((weights[ V > 0 ] - r_min) / r_max)**(1 / a)**a
+                delta[ V < 0 ] = r_3 - r_3 * ((r_3 - weights[ V < 0 ]) / r_3)**(1 / c)**c
+            except:
+                pass
         
         return step_simmpes
 
@@ -141,7 +163,7 @@ def build_mpes( model, pes, rule ):
     # NB "pes" is the "mPES()" frontend class
     
     # Create input error signal
-    error = Signal( shape=rule.size_in, name="PES:error" )
+    error = Signal( shape=rule.size_in, name="mPES:memristors" )
     model.add_op( Reset( error ) )
     model.sig[ rule ][ "in" ] = error  # error connection will attach here
     
@@ -151,14 +173,14 @@ def build_mpes( model, pes, rule ):
     post = get_post_ens( conn )
     encoders = model.sig[ post ][ "encoders" ][ :, conn.post_slice ]
     
-    # create signal to keep track of memristor resistance
-    memristors = Signal( initial_value=np.random.randint( 1e8, 1.1e8, (encoders.shape[ 0 ], acts.shape[ 0 ]) ) )
-    
     model.add_op(
             SimmPES(
-                    acts, error, model.sig[ rule ][ "delta" ], pes.learning_rate, encoders=encoders,
-                    memristor_model=pes.memristor_model,
-                    memristors=pes.memristors
+                    acts,
+                    error,
+                    model.sig[ rule ][ "delta" ],
+                    pes.learning_rate,
+                    encoders,
+                    model.sig[ conn ][ "weights" ]
                     )
             )
     
