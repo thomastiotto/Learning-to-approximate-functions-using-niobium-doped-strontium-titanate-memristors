e56226a9041a49793740f50148f1872a2397a5fb
Thomas <thomas.tiotto@outlook.com>
2020-08-20 08:30:39 +0200

    - added saving output data - added moving average to plot

--- experiments/parameter_search_mPES.py
+++ experiments/parameter_search_mPES.py
@@ -2,14 +2,15 @@ import numpy as np
 from subprocess import run
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
+import pickle
 
 from memristor_nengo.extras import *
 
 # parameters to search
 start_c = -0.0001
 end_c = -1
-num_c = 100
-num_averaging = 5
+num_c = 4
+num_averaging = 1
 
 c_list = np.linspace( start_c, end_c, num=num_c )
 num_parameters = len( c_list )
@@ -17,7 +18,7 @@ print( "Number of parameters:", num_parameters )
 print( "Averaging per parameter", num_averaging )
 print( "Total iterations", num_parameters * num_averaging )
 
-exponents = [ ]
+mse_list = [ ]
 for k, c in enumerate( c_list ):
     print( f"Parameter #{k} ({c})" )
     it_res = [ ]
@@ -26,23 +27,26 @@ for k, c in enumerate( c_list ):
         result = run( [ "python", "mPES.py", "-v", "-d", "1", "-P", str( c ) ],
                       capture_output=True,
                       universal_newlines=True )
+        # print( "Ret", result.returncode )
+        # print( "Out", result.stdout )
+        # print( "Err", result.stderr )
         # save MSE
         mse = float( result.stdout.split()[ 4 ] )
         print( mse )
         it_res.append( mse )
-    exponents.append( it_res )
-    # print( "Ret", result.returncode )
-    # print( "Out", result.stdout )
-    # print( "Err", result.stderr )
+    mse_list.append( it_res )
 
 dir_name, dir_images, dir_data = make_timestamped_dir( root="../data/parameter_search/" )
 
-exp_means = np.mean( exponents, axis=1 )
-plt.plot( c_list, exp_means, label="MSE" )
-exp_means_smooth = np.apply_along_axis( savgol_filter, 0, exp_means, window_length=5, polyorder=3 )
-plt.plot( c_list, exp_means_smooth, label="MSE trend" )
+mse_means = np.mean( mse_list, axis=1 )
+plt.plot( c_list, mse_means, label="MSE" )
+exp_means_smooth = np.convolve( mse_means, np.ones( (mse_means.size,) ) / mse_means.size, mode="same" )
+plt.plot( c_list, exp_means_smooth, label="MSE moving average" )
 
 plt.legend()
 plt.savefig( dir_images + "result" + ".pdf" )
 plt.savefig( dir_images + "result" + ".png" )
 plt.show()
+
+pickle.dump( c_list, open( dir_data + "exponents" + ".pkl", "wb" ) )
+pickle.dump( mse_list, open( dir_data + "mse" + ".pkl", "wb" ) )
