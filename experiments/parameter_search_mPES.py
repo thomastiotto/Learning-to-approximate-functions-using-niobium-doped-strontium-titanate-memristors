3afe89c3f20666083c863262fbdc95d802775bb3
Thomas <thomas.tiotto@outlook.com>
2020-08-20 08:56:14 +0200

    - added parameter passing

--- experiments/parameter_search_mPES.py
+++ experiments/parameter_search_mPES.py
@@ -3,30 +3,47 @@ from subprocess import run
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 import pickle
+import argparse
 
 from memristor_nengo.extras import *
 
+parser = argparse.ArgumentParser()
+parser.add_argument( "-p", "--parameter", choices=[ "exponent", "noise" ], required=True )
+parser.add_argument( "-l", "--limits", nargs=2, type=float, required=True )
+parser.add_argument( "-n", "--number", type=int, required=True )
+parser.add_argument( "-a", "--averaging", type=int, required=True )
+parser.add_argument( "-d", "--directory", default="../data/" )
+args = parser.parse_args()
 # parameters to search
-start_c = -0.0001
-end_c = -1
-num_c = 2
-num_averaging = 2
+parameter = args.parameter
+start_par = args.limits[ 0 ]
+end_par = args.limits[ 1 ]
+num_par = args.number
+num_averaging = args.averaging
+directory = args.directory
 
-c_list = np.linspace( start_c, end_c, num=num_c )
-num_parameters = len( c_list )
+res_list = np.linspace( start_par, end_par, num=num_par )
+num_parameters = len( res_list )
+print( "Evaluation for ", parameter )
+print( f"Search limits of parameters: [{start_par},{end_par}]" )
 print( "Number of parameters:", num_parameters )
 print( "Averaging per parameter", num_averaging )
 print( "Total iterations", num_parameters * num_averaging )
 
 mse_list = [ ]
-for k, c in enumerate( c_list ):
+for k, c in enumerate( res_list ):
     print( f"Parameter #{k} ({c})" )
     it_res = [ ]
     for avg in range( num_averaging ):
         print( f"Averaging #{avg}" )
-        result = run( [ "python", "mPES.py", "-v", "-d", "1", "-P", str( c ) ],
-                      capture_output=True,
-                      universal_newlines=True )
+        if parameter == "exponent":
+            result = run( [ "python", "mPES.py", "-v", "-d", "1", "-P", str( c ) ],
+                          capture_output=True,
+                          universal_newlines=True )
+        if parameter == "noise":
+            result = run( [ "python", "mPES.py", "-v", "-d", "1", "-n", str( c ) ],
+                          capture_output=True,
+                          universal_newlines=True )
         # print( "Ret", result.returncode )
         # print( "Out", result.stdout )
         # print( "Err", result.stderr )
@@ -36,17 +53,17 @@ for k, c in enumerate( c_list ):
         it_res.append( mse )
     mse_list.append( it_res )
 
-dir_name, dir_images, dir_data = make_timestamped_dir( root="../data/parameter_search/" )
+dir_name, dir_images, dir_data = make_timestamped_dir( root=directory + "parameter_search/" )
 
 mse_means = np.mean( mse_list, axis=1 )
-plt.plot( c_list, mse_means, label="MSE" )
+plt.plot( res_list, mse_means, label="MSE" )
 exp_means_smooth = np.convolve( mse_means, np.ones( (mse_means.size,) ) / mse_means.size, mode="same" )
-plt.plot( c_list, exp_means_smooth, label="MSE moving average" )
+plt.plot( res_list, exp_means_smooth, label="MSE average" )
 
 plt.legend()
 plt.savefig( dir_images + "result" + ".pdf" )
 plt.savefig( dir_images + "result" + ".png" )
 plt.show()
 
-pickle.dump( c_list, open( dir_data + "exponents" + ".pkl", "wb" ) )
+pickle.dump( res_list, open( dir_data + "exponents" + ".pkl", "wb" ) )
 pickle.dump( mse_list, open( dir_data + "mse" + ".pkl", "wb" ) )
